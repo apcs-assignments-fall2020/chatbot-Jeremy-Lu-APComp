59735d38896948a478ab2df8099a646a565f70c7
Jeremy-Lu-APComp <21jlu@athenian.org>
2020-09-02 01:25:16 +0000

    Added code to function as a chatbot

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+*/
 default: run
 
 run:
@@ -10,3 +11,4 @@ test:
 	./gradlew clean test
 
 tests: test
+/*
--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,10 +1,25 @@
+//CHATBOT PROGRAM _ Jeremy Lu AP Computer Science -- Assignment 1
 import java.util.Scanner;
 
 public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
+        System.out.print("What is your name? "); // prompts user name
+        String string_Name = scan.next();
+        System.out.println("Nice to meet you " + string_Name + "!"); // outprints greeting
+        System.out.println("What is your favorite number? "); // prompts user favorite color
+        String integer_number = scan.next();
+        System.out.println(string_Name + ", I like " + integer_number + " as well!"); // outprints color_msg
+        System.out.println("What is your favorite food? "); // prompts user favorite food
+        String string_Food = scan.next();
+        System.out.println("That's awesome " + string_Name + " I like " + string_Food + " as well!"); // outprints food_msg
+        System.out.println("What is your favorite time of day? "); // outprints user favorite time
+        String string_daytime = scan.next();
+        System.out.println("Me too! I like " + string_daytime + " as well!"); // outprints day_msg
+        System.out.println("What is your favorite color? "); // prompts user favorite color
+        String string_color = scan.next();
+        System.out.println("I love " + string_color + " as well!"); // outprints user color_msg
         scan.close();
+        System.out.println("Goodbye world!"); // exit
     }
 }
